91a2c77a8766929fef00b317fbf227d8ab507ecb
TreesWise <140375595+TreesWise@users.noreply.github.com>
2024-01-23 10:56:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ async def fetch_data(userinput: demand_forecasting_input, current_user: User = D
 def read_data_from_blob(dataset_name):
     try:
         content = container_client.download_blob(
-            dataset_name).content_as_text(encoding='latin-1')
+            dataset_name).content_as_text()
         data = pd.read_csv(StringIO(content))
         return data
     except Exception as e:
